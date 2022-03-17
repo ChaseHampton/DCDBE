5e0df57178883a7030e9e7c7494e8fc15ae4a92a
Chase Hampton <chase.hampton10@gmail.com>
2022-03-17 10:47:10 -0500

    Use dummy requests for parse process control. Parse items

--- DCDBE/items.py
+++ DCDBE/items.py
@@ -6,7 +6,17 @@
 import scrapy
 
 
-class DcdbeItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class DCDBEItem(scrapy.Item):
+    CertType = scrapy.Field()
+    CertNum = scrapy.Field()
+    CompanyName = scrapy.Field()
+    Address1 = scrapy.Field()
+    Address2 = scrapy.Field()
+    Phone = scrapy.Field()
+    Fax = scrapy.Field()
+    Email = scrapy.Field()
+    Website = scrapy.Field()
+    ContactName = scrapy.Field()
+    ContactTitle = scrapy.Field()
+    Description = scrapy.Field()
+    CertificationAgency = scrapy.Field()
--- DCDBE/spiders/DCDBESpider.py
+++ DCDBE/spiders/DCDBESpider.py
@@ -1,7 +1,11 @@
+import logging
+import re
+
 import scrapy
 from scrapy_splash import SplashRequest
 import sqlite3
 from pathlib import Path
+from ..items import DCDBEItem
 
 
 class DcdbespiderSpider(scrapy.Spider):
@@ -21,6 +25,10 @@ class DcdbespiderSpider(scrapy.Spider):
             yield scrapy.Request(
                 'https://supplier.wmata.com/psp/supplier/SUPPLIER/ERP/c/WT_CL_SEP_MENU.WT_CL_VDR_DIR_CMP.GBL?Page=WT_CL_VDR_DIR_PG&Action=U',
                 dont_filter=True, callback=self.collect_data)
+        else:
+            yield scrapy.Request(
+                'https://supplier.wmata.com/psp/supplier/SUPPLIER/ERP/c/WT_CL_SEP_MENU.WT_CL_VDR_DIR_CMP.GBL?Page=WT_CL_VDR_DIR_PG&Action=U',
+                dont_filter=True, callback=self.process_responses)
 
     def collect_data(self, response):
         script = """
@@ -38,11 +46,13 @@ class DcdbespiderSpider(scrapy.Spider):
           local maxnum = maxpage:text()
           i, j = maxnum:find('%d+$')
           local maxcount = string.sub(maxnum, i ,j)
-          for i=1,tonumber(maxcount)/100,1
+          for i=1,(tonumber(maxcount)/100)+1,1
           do
             local nextpage = splash:select('a#WT_CL_VDRDIR_VW\\\\$hdown\\\\$0')
             pages[i] = splash:html()
-            assert(nextpage:mouse_click())
+            if nextpage then
+              assert(nextpage:mouse_click())
+            end
             assert(splash:wait(1.0))
           end
           return {
@@ -63,14 +73,75 @@ class DcdbespiderSpider(scrapy.Spider):
             curs.execute(insert_query, (k, v))
         self.db_conn.commit()
         curs.close()
+        req = scrapy.Request(url='http://example.com', dont_filter=True, callback=self.process_responses)
+        yield req
 
-    def process_responses(self):
+    def process_responses(self, response):
         curs = self.db_conn.cursor()
         select_query = """SELECT page, response FROM responses"""
         curs.execute(select_query)
         pages = curs.fetchall()
+        req = scrapy.Request(url='http://example.com', dont_filter=True, meta={'page': pages}, callback=self.parse_response_page)
+        yield req
+
+    def parse_response_page(self, response):
+        pages = response.meta['page']
         for page in pages:
-            pass
+            sel = scrapy.Selector(text=page[-1], type='html')
+            rows = sel.xpath('//tr[contains(@id, "_row")]')
+            curs = self.db_conn.cursor()
+            # try:
+            for row in rows:
+                items = DCDBEItem()
+                item_dict = {'cert_type': row.xpath('.//span[contains(@id, "_DESCR20")]/text()').getall(),
+                             'cert_num': row.xpath('.//span[contains(@id, "_CERTSERIAL")]/text()').getall(),
+                             'company': row.xpath('.//span[contains(@id, "_NAME1")]/text()').getall(), 'address1': '',
+                             'address2': '', 'name': row.xpath('.//span[contains(@id, "_CONTACT_NAME")]/text()').getall(),
+                             'title': row.xpath('.//span[contains(@id, "_CONTACT_TITLE")]/text()').getall(),
+                             'desc': row.xpath('.//span[contains(@id, "_DESCRLONG")]/text()').getall(),
+                             'cert_agency': row.xpath('.//span[contains(@id, "_CERTIF")]/text()').getall()}
+                address_full = row.xpath('.//span[contains(@id, "_ADDRESS")]/text()').getall()
+                item_dict['address1'], item_dict['address2'], item_dict['phone'], item_dict['fax'], \
+                    item_dict['email'], item_dict['website'] = self.parse_address(address_full)
+                items['CertType'] = self.clean_item(item_dict['cert_type'])
+                items['CertNum'] = self.clean_item(item_dict['cert_num'])
+                items['CompanyName'] = self.clean_item(item_dict['company'])
+                items['Address1'] = self.clean_item(item_dict['address1'])
+                items['Address2'] = self.clean_item(item_dict['address2'])
+                items['Phone'] = self.clean_item(item_dict['phone'])
+                items['Fax'] = self.clean_item(item_dict['fax'])
+                items['Email'] = self.clean_item(item_dict['email'])
+                items['Website'] = self.clean_item(item_dict['website'])
+                items['ContactName'] = self.clean_item(item_dict['name'])
+                items['ContactTitle'] = self.clean_item(item_dict['title'])
+                items['Description'] = self.clean_item(item_dict['desc'])
+                items['CertificationAgency'] = self.clean_item(item_dict['cert_agency'])
+                yield items
+            curs.execute("""DELETE FROM responses WHERE page = ?""", (page[0],))
+            self.db_conn.commit()
+            self.logger.info(f"Successfully parsed page {page[0]}.")
+
+    def parse_address(self, address_full: list):
+        address1 = address_full[0].strip()
+        address2 = address_full[1].strip()
+        phone = [v.replace("Phone: ", "") for v in address_full if 'Phone:' in v]
+        fax = [v.replace("Fax: ", "") for v in address_full if 'Fax:' in v]
+        email = [v.replace("Email: ", "") for v in address_full if 'Email:' in v]
+        website = [v.replace("Website: ", "") for v in address_full if 'Website:' in v]
+        return address1, address2, phone, fax, email, website
+
+    def clean_item(self, item) -> str:
+        if not item:
+            return ''
+        if type(item) == list:
+            if len(item) == 1:
+                return item[0].strip()
+            elif len(item) > 1 and re.match(r'\d{6}', item[0]):
+                return ";".join([v.strip() for v in item])
+            else:
+                return " ".join([v.strip() for v in item])
+        else:
+            return item.strip()
 
     def create_table(self):
         create_string = """
@@ -81,6 +152,8 @@ class DcdbespiderSpider(scrapy.Spider):
         );
         """
         curs = self.db_conn.cursor()
+        # curs.execute("""DROP TABLE responses""")
+        # self.db_conn.commit()
         curs.execute(create_string)
         self.db_conn.commit()
         curs.close()
